6547402cfbf0ac6dad36a8ca66271557dcf771fd
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-26 21:37:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,11 @@
 class Dog
-  def name= (dog_name)
-    @this_dogs_name = dogs_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
-end
 
 def name = (fido)
   @name = fido
